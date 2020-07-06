4c182a93fb4d4a70939d18aa5fbf9dc73ea6ec22
Stefan Zabka <zabkaste@informatik.hu-berlin.de>
2020-07-06 12:38:53 +0200

    Wrapped get_matching rules to bind blockers in scope

--- openwpm_utils/blocklist.py
+++ openwpm_utils/blocklist.py
@@ -1,25 +1,16 @@
 from urllib.parse import urlparse
 import domain_utils as du
+from typing import List
 
-import abp_blocklist_parser
 import requests
 import pyspark.sql.functions as F
 from pyspark.sql.types import *
 
+from abp_blocklist_parser import BlockListParser
 
-# Download blocklists
-raw_lists = {
-  'nocoin': 'https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/nocoin.txt',
-  'ublock-resource-abuse': 'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/resource-abuse.txt'
-}
-# Initialize parsers
-blockers = [
-  abp_blocklist_parser.BlockListParser(regexes=requests.get(raw_lists["nocoin"]).content.decode()),
-  abp_blocklist_parser.BlockListParser(regexes=requests.get(raw_lists["ublock-resource-abuse"]).content.decode())
-]
 # Mapping from
 # https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/webRequest/ResourceType
-# To 
+# To
 # https://help.eyeo.com/en/adblockplus/how-to-write-filters#options
 # With help from
 # https://github.com/gorhill/uBlock/blob/1d5800629aaca0d152127d844ca7f6cf975f2f68/src/js/static-net-filtering.js#L55
@@ -30,28 +21,29 @@ blockers = [
 # to generate an object that represents the direct mapping between these types
 
 type_to_option = {
-"beacon":"ping",
-"csp_report":"other",
-"font":"font",
-"image":"image",
-"imageset":"image",
-"main_frame":"document",
-"media":"media",
-"object":"object",
-"object_subrequest":"object",
-"ping":"ping",
-"script":"script",
-"speculative":"other",
-"stylesheet":"stylesheet",
-"sub_frame":"subdocument",
-"web_manifest":"other",
-"websocket":"websocket",
-"xbl":"other",
-"xml_dtd":"other",
-"xmlhttprequest":"xmlhttprequest",
-"xslt":"other",
-"other":"other"
+    "beacon": "ping",
+    "csp_report": "other",
+    "font": "font",
+    "image": "image",
+    "imageset": "image",
+    "main_frame": "document",
+    "media": "media",
+    "object": "object",
+    "object_subrequest": "object",
+    "ping": "ping",
+    "script": "script",
+    "speculative": "other",
+    "stylesheet": "stylesheet",
+    "sub_frame": "subdocument",
+    "web_manifest": "other",
+    "websocket": "websocket",
+    "xbl": "other",
+    "xml_dtd": "other",
+    "xmlhttprequest": "xmlhttprequest",
+    "xslt": "other",
+    "other": "other",
 }
+
 
 def get_option_dict(url, top_level_url, resource_type=None):
     """Build an options dict for BlockListParser.
@@ -81,30 +73,31 @@ def get_option_dict(url, top_level_url, resource_type=None):
             options[type_to_option[resource_type]] = True
         except KeyError:
             raise ValueError(
-                "Argument %s given for `resource_type` not found in map." % resource_type
+                "Argument %s given for `resource_type` not found in map."
+                % resource_type
             )
     options["domain"] = urlparse(top_level_url).hostname
-    
+
     # Add third-party option if third party. Value doesn't matter.
     if du.get_ps_plus_1(url) != du.get_ps_plus_1(top_level_url):
         options["third-party"] = True
     return options
 
-def get_matching_rules(url, top_level_url, resource_type):
-  # skip top-level requests
-  if top_level_url is None:
-    return
-  
-  matching_rules = set()
-  options = get_option_dict(url, top_level_url, resource_type)
-  
-  for blocker in blockers:
-    result = blocker.should_block_with_items(url, options=options)
-    if result is not None and result[0] == 'blacklisted':
-      matching_rules = matching_rules.union(result[1])
-    if len(matching_rules) > 0:
-      return tuple(matching_rules)
-  return
 
+def prepare_get_matching_rules(blockers: List[BlockListParser]):
+    def get_matching_rules(url, top_level_url, resource_type):
+        # skip top-level requests
+        if top_level_url is None:
+            return
 
-udf_get_matching_rules = F.udf(get_matching_rules, ArrayType(StringType()))
+        matching_rules = set()
+        options = get_option_dict(url, top_level_url, resource_type)
+
+        for blocker in blockers:
+            result = blocker.should_block_with_items(url, options=options)
+            if result is not None and result[0] == "blacklisted":
+                matching_rules = matching_rules.union(result[1])
+            if len(matching_rules) > 0:
+                return tuple(matching_rules)
+
+    return F.udf(get_matching_rules, ArrayType(StringType()))
