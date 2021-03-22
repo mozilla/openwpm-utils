a8003ffc8a6d4a9b3acc05dc18ac1181feebefd0
Stefan Zabka <szabka@mozilla.com>
2021-03-22 18:21:06 +0100

    Changing scheme from gcs to gs

--- openwpm_utils/gcs.py
+++ openwpm_utils/gcs.py
@@ -7,13 +7,16 @@ import pyarrow.parquet as pq
 import pyspark.sql.functions as F
 from google.cloud import storage
 from pyspark.context import SparkContext
-from pyspark.sql import SQLContext
+from pyspark.sql import SQLContext, DataFrame
+from pandas import DataFrame as PandasDataFrame
 
 from openwpm_utils.crawlhistory import get_worst_status_per_visit_id
 
 
 class GCSDataset(object):
-    def __init__(self, base_dir: str, bucket:Optional[str]="openwpm-data", **kwargs) -> None:
+    def __init__(
+        self, base_dir: str, bucket: Optional[str] = "openwpm-data", **kwargs
+    ) -> None:
         """Helper class to load OpenWPM datasets from GCS using pandas
 
         This dataset wrapper is safe to use by spark worker processes, as it
@@ -35,8 +38,8 @@ class GCSDataset(object):
         self._content_key = f"{base_dir}/content/%s.gz"
         self._gcsfs = gcsfs.GCSFileSystem(**kwargs)
 
-    def read_table(self, table_name, columns=None):
-        """Read `table_name` from OpenWPM dataset into a pyspark dataframe.
+    def read_table(self, table_name: str, columns: List[str]=None) -> PandasDataFrame:
+        """Read `table_name` from OpenWPM dataset into a pandas dataframe.
 
         Parameters
         ----------
@@ -48,18 +51,19 @@ class GCSDataset(object):
         return (
             pq.ParquetDataset(
                 self._table_location_format_string % table_name,
-                filesystem= self._gcsfs,
+                filesystem=self._gcsfs,
                 metadata_nthreads=4,
             )
             .read(use_pandas_metadata=True, columns=columns)
             .to_pandas()
         )
 
-    def collect_content(self, content_hash: str, beautify: bool=False) -> Optional[Union[bytes, str]]:
+    def collect_content(
+        self, content_hash: str, beautify: bool = False
+    ) -> Optional[Union[bytes, str]]:
         """Collect content by directly connecting to GCS via google.cloud.storage"""
         storage_client = storage.Client()
         bucket = storage_client.bucket(self._bucket)
-
 
         blob = bucket.blob(self._content_key % content_hash)
         content: Union[bytes, str] = blob.download_as_bytes()
@@ -71,8 +75,15 @@ class GCSDataset(object):
                 pass
         return content
 
+
 class PySparkGCSDataset(GCSDataset):
-    def __init__(self, spark_context: SparkContext, base_dir: str, bucket:str="openwpm-data", **kwargs) -> None:
+    def __init__(
+        self,
+        spark_context: SparkContext,
+        base_dir: str,
+        bucket: str = "openwpm-data",
+        **kwargs,
+    ) -> None:
         """Helper class to load OpenWPM datasets from GCS using PySpark
 
         Parameters
@@ -88,7 +99,9 @@ class PySparkGCSDataset(GCSDataset):
         super().__init__(base_dir, bucket, **kwargs)
         self._spark_context = spark_context
         self._sql_context = SQLContext(spark_context)
-        self._table_location_format_string = f"gcs://{self._table_location_format_string}"
+        self._table_location_format_string = (
+            f"gs://{self._table_location_format_string}"
+        )
         self._incomplete_visit_ids = self.read_table(
             "incomplete_visits", mode="all"
         ).select("visit_id")
@@ -100,8 +113,8 @@ class PySparkGCSDataset(GCSDataset):
         )
 
     def read_table(
-        self, table_name: str, columns: List[str] = None, mode: str = "successful"
-    ):
+        self, table_name: str, columns: Optional[List[str]] = None, mode: str = "successful"
+    ) -> DataFrame:
         """Read `table_name` from OpenWPM dataset into a pyspark dataframe.
 
         Parameters
@@ -115,7 +128,9 @@ class PySparkGCSDataset(GCSDataset):
             Success is determined per visit_id. A visit_id is failed
             if one of it's commands failed or if it's in the interrupted table
         """
-        table = self._sql_context.read.parquet(self._table_location_format_string % table_name)
+        table = self._sql_context.read.parquet(
+            self._table_location_format_string % table_name
+        )
         if columns is not None:
             table = table.select(columns)
         if mode == "all":
